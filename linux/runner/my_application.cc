87780b567babd1cc252dde1178cfd603f5acf806
jim <fqcwork@163.com>
2025-12-29 10:53:31 +0800

    feat:修改应用名称

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -46,11 +46,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "bug_out");
+    gtk_header_bar_set_title(header_bar, "Bug Out");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "bug_out");
+    gtk_window_set_title(window, "Bug Out");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
